89d3f514bd7ca3ad0cecc0f8709cfdda9369f38f
Alexandre Leblanc <alexandre.leblanc@savoirfairelinux.com>
2017-04-11 14:11:06 -0400

    cukinia: new package
    Cukinia is designed to help Linux-based embedded firmware developers
    run simple validation tests on their firmware. For basic usage it
    requires no dependencies other than busybox.
    
    Cukinia can run tests at boot time which is why we are using SZZcukinia
    since it would possibly be the last script to be executed in 'init.d'.
    
    Change-Id: I04a56f59189be7c2f8b8b81ee9730c9fc97b62c7
    Signed-off-by: Alexandre Leblanc <alexandre.leblanc@savoirfairelinux.com>

---
+++ package/cukinia/cukinia.mk
@@ -0,0 +1,26 @@
+################################################################################
+#
+# cukinia
+#
+################################################################################
+
+CUKINIA_VERSION = 1e43172408e57140a5b5d84e8572a8eede1b8471
+CUKINIA_SITE = $(call github,savoirfairelinux,cukinia,$(CUKINIA_VERSION))
+CUKINIA_LICENSE = Apache-2.0 or GPLv3
+
+define CUKINIA_INSTALL_TARGET_CMDS
+	$(INSTALL) -D -m 0755 $(@D)/cukinia \
+		$(TARGET_DIR)/usr/bin/cukinia
+
+	$(INSTALL) -D -m 0644 $(CUKINIA_PKGDIR)/cukinia.conf \
+		$(TARGET_DIR)/etc/cukinia/cukinia.conf
+endef
+
+ifeq ($(BR2_PACKAGE_CUKINIA_BOOT),y)
+define CUKINIA_INSTALL_INIT_SYSV
+	$(INSTALL) -D -m 0755 $(CUKINIA_PKGDIR)/SZZcukinia \
+		$(TARGET_DIR)/etc/init.d/SZZcukinia
+endef
+endif
+
+$(eval $(generic-package))
