4041e56ac579501dcd0bdeda851ed0a5162c36f4
Alexandre Leblanc <alexandre.leblanc@savoirfairelinux.com>
2017-05-16 12:16:28 -0400

    altera_sockit: add mkpimage
    The altera needs a signed image in order to boot.
    
    Add backport support and activate mkpimage to generate a
    signature for the altera board.
    
    This backport come from buildroot
    af31c309e73ca88ee70c52e591f90e4b89ff5e55
    
    Issue: #1644
    Change-Id: I8a2072957bbca02c719d1ea1a82083db5b9d8a42

--- board/altera/sockit/post-image.sh
+++ board/altera/sockit/post-image.sh
@@ -2,6 +2,19 @@
 # post-image.sh for SoCkit
 # 2014, "Roman Diouskine" <roman.diouskine@savoirfairelinux.com>
 # 2014, "Sebastien Bourdelin" <sebastien.bourdelin@savoirfairelinux.com>
+# 2017. "Alexandre Leblanc" <alexandre.leblanc@savoirfairelinux.com>
+
+set -e
 
 # create a DTB file copy with the name expected by the u-boot config
 cp -af $BINARIES_DIR/socfpga_cyclone5_sockit.dtb  $BINARIES_DIR/socfpga.dtb
+
+BOARD_DIR=$(dirname $0)
+
+mkpimage $BINARIES_DIR/u-boot-spl.bin -o $BINARIES_DIR/u-boot-spl.bin.crc
+
+# Create SPL + bootloader image
+fallocate -l 1M $BINARIES_DIR/uboot-part.img
+dd if=$BINARIES_DIR/u-boot-spl.bin of=$BINARIES_DIR/uboot-part.img bs=64k seek=0
+dd if=$BINARIES_DIR/u-boot-spl.bin.crc of=$BINARIES_DIR/uboot-part.img bs=64k seek=1
+dd if=$BINARIES_DIR/u-boot.img of=$BINARIES_DIR/uboot-part.img bs=64k seek=4
