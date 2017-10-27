c99994ae77467741fa7cd39a51853316b9dded71
Alexandre Leblanc <alexandre.leblanc@savoirfairelinux.com>
2017-10-27 14:45:40 -0400

    [wip] modification for futur altera patch
    no issue
    
    Change-Id: I449a9c36717ece6313e2312d3390069caf20d1d7

--- board/altera/sockit/post-image.sh
+++ board/altera/sockit/post-image.sh
@@ -2,11 +2,11 @@
 # post-image.sh for SoCkit
 # 2014, "Roman Diouskine" <roman.diouskine@savoirfairelinux.com>
 # 2014, "Sebastien Bourdelin" <sebastien.bourdelin@savoirfairelinux.com>
-# 2017. "Alexandre Leblanc" <alexandre.leblanc@savoirfairelinux.com>
 
-set -e
+BOARD_DIR="$(dirname $0)"
+GENIMAGE_CFG="${BOARD_DIR}/genimage.cfg"
+GENIMAGE_TMP="${BUILD_DIR}/.genimage_tmp"
 
-# create a DTB file copy with the name expected by the u-boot config
 cp -af $BINARIES_DIR/socfpga_cyclone5_sockit.dtb  $BINARIES_DIR/socfpga.dtb
 
 BOARD_DIR=$(dirname $0)
@@ -18,3 +18,10 @@ fallocate -l 1M $BINARIES_DIR/uboot-part.img
 dd if=$BINARIES_DIR/u-boot-spl.bin of=$BINARIES_DIR/uboot-part.img bs=64k seek=0
 dd if=$BINARIES_DIR/u-boot-spl.bin.crc of=$BINARIES_DIR/uboot-part.img bs=64k seek=1
 dd if=$BINARIES_DIR/u-boot.img of=$BINARIES_DIR/uboot-part.img bs=64k seek=4
+
+${HOST_DIR}/usr/bin/genimage \
+	--config ${GENIMAGE_CFG} \
+	--rootpath ${TARGET_DIR} \
+	--tmppath ${GENIMAGE_TMP} \
+	--inputpath ${BINARIES_DIR} \
+	--outputpath ${BINARIES_DIR}
